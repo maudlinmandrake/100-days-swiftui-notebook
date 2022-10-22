2e7cee126869ed1de6d8d8ec2b3582271642656d
Jenny Mikac <jennymikac@gmail.com>
2022-10-22 15:50:42 -0400

    Checkpoint 5

---
+++ checkpoint-5.playground/Contents.swift
@@ -0,0 +1,15 @@
+//
+// 100 Days of SwiftUI: Checkpoint 5, Closures
+// Jenny Mikac
+//
+// The challenge:
+// Take this array: let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49], and...
+// 1. Filter out all even numbers
+// 2. Sort in ascending order
+// 3. Map to strings in format of "__ is a lucky number"
+// 4. Print resulting array line by line
+//
+
+import Cocoa
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
